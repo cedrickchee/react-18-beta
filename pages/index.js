6186a32b2efc052f5ac0b0cfd640aed82fe5c07b
Cedric Chee <cedric@invictusbyte.com>
2021-11-27 16:10:16 +0800

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+function HomePage() {
+  return <div>Welcome to Calendso!</div>
+}
+
+export default HomePage;
