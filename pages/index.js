346f03b84c684087cbc7c3065b71c2c53b74cd65
Cedric Chee <cedric@invictusbyte.com>
2021-11-28 10:30:56 +0800

    revert filename change

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-function HomePage() {
-  return <div>Welcome to Calendso!</div>
-}
-
-export default HomePage;
